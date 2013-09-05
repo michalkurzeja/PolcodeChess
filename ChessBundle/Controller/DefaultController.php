c72b51740cbc30a7a092b1f088983e8ee1601f31
michalkurzeja <michal.kurzeja@polcode.net>
2013-09-05 15:30:39 +0200

    gittest

--- ChessBundle/Controller/DefaultController.php
+++ ChessBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        $chess;
         return $this->render('PolcodeChessBundle:Default:index.html.twig', array('name' => $name));
     }
 }
